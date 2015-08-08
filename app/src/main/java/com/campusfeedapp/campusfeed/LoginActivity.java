fe1cdcebb13438c548889380fa582d1ee8847d27
Chinmay Joshi <chinmay@chinmay-300E4C-300E5C-300E7C.(none)>
2015-08-08 12:02:26 +0530

    nav drawer and tab implemented

--- app/src/main/java/com/campusfeedapp/campusfeed/LoginActivity.java
+++ app/src/main/java/com/campusfeedapp/campusfeed/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.campusfeedapp.campusfeed;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class LoginActivity extends ActionBarActivity {
 
+    RobotoTextView loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn = (RobotoTextView) findViewById(R.id.submit_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
